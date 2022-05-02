356ce9068d7b34f5f6d39ca5caae5241f1a54692
Jonasthj <jonasthenderupjensen@gmail.com>
2022-05-02 20:35:36 +0200

    react router with endpoints

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {BrowserRouter, Route, Routes, Link} from "react-router-dom";
 
 
+function FrontPage() {
+    return <div>
+        <div><Link to="/login">Login</Link></div>
+        <div><Link to="/profile">Profile</Link></div>
+    </div>;
+}
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById("app"));
+function Application() {
+    return <BrowserRouter>
+        <Routes>
+            <Route path={"/"} element={<FrontPage/>} />
+            <Route path={"/login"} element={<h1>Login</h1>} />
+            <Route path={"/login/callback"} element={<h1>Login Callback</h1>} />
+            <Route path={"/profile"} element={<h1>Profile</h1>} />
+        </Routes>
+    </BrowserRouter>;
+}
+
+ReactDOM.render(<Application/>, document.getElementById("app"));
