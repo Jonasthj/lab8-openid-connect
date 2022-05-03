c5720edad86d1819fd6919927a071a108450ac63
Jonasthj <jonasthenderupjensen@gmail.com>
2022-05-03 14:09:05 +0200

    Get access token and store in cookie

--- client/index.jsx
+++ client/index.jsx
@@ -1,24 +1,82 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter, Route, Routes, Link} from "react-router-dom";
-
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
 function FrontPage() {
-    return <div>
-        <div><Link to="/login">Login</Link></div>
-        <div><Link to="/profile">Profile</Link></div>
-    </div>;
+  return (
+    <div>
+      <div>
+        <Link to="/login">Login</Link>
+      </div>
+      <div>
+        <Link to="/profile">Profile</Link>
+      </div>
+    </div>
+  );
+}
+
+async function fetchJSON(url) {
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error();
+  }
+  return await res.json();
+}
+
+function Login() {
+  const [redirectUrl, setRedirectUrl] = useState();
+
+  useEffect(async () => {
+    const { authorization_endpoint } = await fetchJSON(
+      "https://accounts.google.com/.well-known/openid-configuration"
+    );
+    console.log(authorization_endpoint);
+
+    const parameters = {
+      response_type: "token",
+      client_id:
+        "198418837829-3v54q89im2g82tohg2u74ss21q7559v4.apps.googleusercontent.com",
+      scope: "email profile",
+      redirect_uri: window.location.origin + "/login/callback",
+    };
+    window.location.href =
+      authorization_endpoint + "?" + new URLSearchParams(parameters);
+  }, []);
+  return (
+    <div>
+      <h1>Please Wait...</h1>
+    </div>
+  );
+}
+
+function LoginCallback() {
+  useEffect(async () => {
+    const { access_token } = Object.fromEntries(
+      new URLSearchParams(window.location.hash.substring(1))
+    );
+    await fetch("/api/login", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ access_token }),
+    });
+  });
+
+  return <h1>Login Callback</h1>;
 }
 
 function Application() {
-    return <BrowserRouter>
-        <Routes>
-            <Route path={"/"} element={<FrontPage/>} />
-            <Route path={"/login"} element={<h1>Login</h1>} />
-            <Route path={"/login/callback"} element={<h1>Login Callback</h1>} />
-            <Route path={"/profile"} element={<h1>Profile</h1>} />
-        </Routes>
-    </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<FrontPage />} />
+        <Route path={"/login"} element={<Login />} />
+        <Route path={"/login/callback"} element={<LoginCallback />} />
+        <Route path={"/profile"} element={<h1>Profile</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-ReactDOM.render(<Application/>, document.getElementById("app"));
+ReactDOM.render(<Application />, document.getElementById("app"));
--- server/server.js
+++ server/server.js
@@ -1,19 +1,32 @@
 import express from "express";
 import * as path from "path";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
 
+dotenv.config();
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+app.post("/api/login", (req, res, next) => {
+  const { access_token } = req.body;
+  res.cookie("access_token", access_token, { signed: true });
+  res.sendStatus(200);
+});
 
 app.use(express.static("../client/dist"));
 
 app.use((req, res, next) => {
-    if(req.method === "GET" && !req.path.startsWith("/api")){
-        res.sendFile(path.resolve("../client/dist/index.html"));
-    } else {
-        next();
-    }
+  if (req.method === "GET" && !req.path.startsWith("/api")) {
+    res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    next();
+  }
 });
 
 const server = app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server started on http://localhost:${server.address().port}`)
+  console.log(`Server started on http://localhost:${server.address().port}`);
 });
